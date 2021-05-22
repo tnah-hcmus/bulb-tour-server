ed257af023f54cf3135385e64a1e53af145bbab1
tvp-github <tavinhphuc.ctg@gmail.com>
2021-05-22 23:30:43 +0700

    + update getNearBy with ngeohash

--- controllers/location/location.service.js
+++ controllers/location/location.service.js
@@ -1,53 +1,56 @@
 const { location: Location } = require("models/index");
-
+const geohash = require("ngeohash");
+const { Op } = require("sequelize");
 module.exports = {
-  create,
-  update,
-  updateRating,
-  delete: _delete,
-  getByLocationId,
-  getNearby
+	create,
+	update,
+	updateRating,
+	delete: _delete,
+	getByLocationId,
+	getNearby,
 };
 
 async function create(params) {
-  try {
-    const location = new Location(params);
-    // save payment
-    await location.save();
-    return location;
-  } catch (err) {
-    throw err;
-  }
+	try {
+		//params.hash = createHash(params)
+		params.hash = geohash.encode(params.lat, params.long, 9);
+		const location = new Location(params);
+		// save payment
+		await location.save();
+		return location;
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function update(id, params) {
-  try {
-    const location = await getByLocationId(id);
-    // copy params to account and save
-    Object.assign(location, params);
-    await location.save();
-  } catch (err) {
-    throw err;
-  }
+	try {
+		const location = await getByLocationId(id);
+		// copy params to account and save
+		Object.assign(location, params);
+		await location.save();
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function _delete(id) {
-  try {
-    const location = await getByLocationId(id);
-    await location.destroy();
-  } catch (err) {
-    throw err;
-  }
+	try {
+		const location = await getByLocationId(id);
+		await location.destroy();
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function getByLocationId(id) {
-  try {
-    const location = await Location.findByPk(id);
-    if (!location) throw "Location not found";
-    return location;
-  } catch (err) {
-    throw err;
-  }
+	try {
+		const location = await Location.findByPk(id);
+		if (!location) throw "Location not found";
+		return location;
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function getByHash(hash) {
@@ -61,27 +64,32 @@ async function getByHash(hash) {
 }
 
 async function getNearby({ lat, long }) {
-  try {
-    const locations = await Location.findAll();
-    return locations;
-  } catch (err) {
-    err;
-  }
+	try {
+		const level = 6;
+    //config nearby level or nearby distance
+		const neighbors = geohash.neighbors(geohash.encode(lat, long, level));
+    const options = neighbors.map((geoString)={[Op.startsWith]:geoString})
+		const locations = await Location.findAll({ where: { [Op.or]: options } });
+		return locations;
+	} catch (err) {
+		err;
+	}
 }
 
 async function updateRating(id, rating, isNew, oldRating) {
-  try {
-    const location = await getByLocationId(id);
-    const totalRating = location.rating * location.ratingNumber;
-    if (isNew) {
-      location.rating = (totalRating + rating) / (location.ratingNumber + 1);
-      location.ratingNumber++;
-    } else {
-      location.rating =
-        (totalRating + rating - oldRating) / location.ratingNumber;
-    }
-    await location.save();
-  } catch (err) {
-    throw err;
-  }
+	try {
+		const location = await getByLocationId(id);
+		const totalRating = location.rating * location.ratingNumber;
+		if (isNew) {
+			location.rating =
+				(totalRating + rating) / (location.ratingNumber + 1);
+			location.ratingNumber++;
+		} else {
+			location.rating =
+				(totalRating + rating - oldRating) / location.ratingNumber;
+		}
+		await location.save();
+	} catch (err) {
+		throw err;
+	}
 }
