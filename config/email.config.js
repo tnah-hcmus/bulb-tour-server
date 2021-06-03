ad079c616ccbc83aba5373f387c8dabb3e779627
hoangtna <hoangtna@vng.com.vn>
2021-06-03 10:49:34 +0700

    email setup

--- config/email.config.js
+++ config/email.config.js
@@ -2,10 +2,14 @@ module.exports = {
   emailFrom: "tnah@bulbtour.com",
   smtpOptions: {
     host: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     auth: {
+      type: 'OAuth2',
       user: "yellowdragon1999qn@gmail.com",
-      pass: process.env.MAIL_PASS,
+      clientId: '586327316542-9mtefisavm8ula4d4thbdu1391i2dhu5.apps.googleusercontent.com',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_CLIENT_REFRESH,
+      accessToken: process.env.GOOGLE_CLIENT_TOKEN
     },
   },
 };
