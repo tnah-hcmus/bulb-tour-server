48b4afa985feb56000d1b7c05ff487d3fbc54d7e
hoangtna <hoangtna@vng.com.vn>
2021-06-03 10:55:32 +0700

    email setup

--- config/email.config.js
+++ config/email.config.js
@@ -1,15 +1,17 @@
+const xoauth2 = require('xoauth2')
 module.exports = {
   emailFrom: "tnah@bulbtour.com",
-  smtpOptions: {
-    host: "smtp.gmail.com",
+  service: 'gmail',
+  auth: {
     port: 465,
     auth: {
       type: 'OAuth2',
-      user: "yellowdragon1999qn@gmail.com",
-      clientId: '586327316542-9mtefisavm8ula4d4thbdu1391i2dhu5.apps.googleusercontent.com',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_CLIENT_REFRESH,
-      accessToken: process.env.GOOGLE_CLIENT_TOKEN
     },
+    xoauth2: xoauth2.createXOAuth2Generator({
+        user: "yellowdragon1999qn@gmail.com",
+        clientId: '586327316542-9mtefisavm8ula4d4thbdu1391i2dhu5.apps.googleusercontent.com',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_CLIENT_REFRESH,
+    })
   },
 };
