20ea810a64f25d53849e8b0fbd68b65e3eeae75e
hoangtna <hoangtna@vng.com.vn>
2021-06-03 11:03:07 +0700

    email setup

--- config/email.config.js
+++ config/email.config.js
@@ -1,12 +1,7 @@
 const xoauth2 = require('xoauth2')
 module.exports = {
-  emailFrom: "tnah@bulbtour.com",
   service: 'gmail',
   auth: {
-    port: 465,
-    auth: {
-      type: 'OAuth2',
-    },
     xoauth2: xoauth2.createXOAuth2Generator({
         user: "yellowdragon1999qn@gmail.com",
         clientId: '586327316542-9mtefisavm8ula4d4thbdu1391i2dhu5.apps.googleusercontent.com',
